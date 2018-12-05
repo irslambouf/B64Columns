5d6e648f5c7000666fba7f904e6061996e9f9d9b
Philippe Fisher <phil.the.fish@gmail.com>
2018-12-05 17:17:13 -0500

    First

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+import org.apache.commons.text.StringEscapeUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Base64;
+
+public class Main {
+    public static void main(String[] args){
+        if (args.length < 3){
+            System.err.println("Usage:java -jar B64Columns [delimiter] [in filename] [out filename]");
+            return;
+        }
+
+        byte[] delimiter = StringEscapeUtils.unescapeJava(args[0]).getBytes();
+
+        if (delimiter.length == 0){
+            System.err.println("Delimiter length is 0, exiting...");
+            return;
+        }
+
+        File in = new File(args[1]);
+        if (!in.exists()){
+            System.err.println("Input file does not exist, exiting...");
+            return;
+        }
+
+        File out = new File(args[2]);
+        if (out.exists()){
+            Scanner scanner = new Scanner(System.in);
+            String input = null;
+            do {
+                System.out.print("Output file already exists, do you want to override (Y/N)? ");
+                input = scanner.next();
+            } while (input == null || !(input.equals("Y") || input.equals("N")));
+
+            if (input.equalsIgnoreCase("N")) {
+                System.out.println("File will not be overwritten, exiting...");
+                return;
+            }
+        }
+
+        try {
+            FileOutputStream fos = new FileOutputStream(out);
+            FileAndFolderReaderBinaryV2 reader = new FileAndFolderReaderBinaryV2(args[1]);
+
+            byte[] line;
+            while ((line = reader.readLine()) != null){
+                UtilBytes uLine = new UtilBytes(line);
+
+                ArrayList<Integer> indexes = uLine.getAllIndexes(delimiter);
+
+                for (int i=0; i< indexes.size(); i++){
+                    if (i==0){
+                        fos.write(Base64.getEncoder().encode(uLine.getBytesOfRange(0, indexes.get(i))));
+                    }else {
+                        fos.write(Base64.getEncoder().encode(uLine.getBytesOfRange(indexes.get(i-1)+delimiter.length, indexes.get(i))));
+                    }
+
+                    fos.write(delimiter);
+                }
+                fos.write(Base64.getEncoder().encode(uLine.getBytesOfRange(indexes.get(indexes.size()-1)+delimiter.length, uLine.size()-1)));
+                fos.write("\n".getBytes());
+                fos.flush();
+            }
+        } catch (IOException e) {
+            System.err.println("Output file can't be written to, exiting...");
+        }
+    }
+}
