a07e7e5b6680962e5e6376cf3685e35aeed004b3
Philippe Fisher <phil.the.fish@gmail.com>
2018-12-20 16:40:04 -0500

    Update with sha256

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import org.apache.commons.text.StringEscapeUtils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Base64;
 
 public class Main {
     public static void main(String[] args){
+        long start = System.nanoTime();
         if (args.length < 3){
             System.err.println("Usage:java -jar B64Columns [delimiter] [in filename] [out filename]");
             return;
@@ -47,43 +49,83 @@ public class Main {
             FileOutputStream fos = new FileOutputStream(out);
             FileAndFolderReaderBinaryV2 reader = new FileAndFolderReaderBinaryV2(args[1]);
             Base64.Encoder encoder = Base64.getEncoder();
+            MessageDigest sha256 = null;
+            try{
+                sha256 = MessageDigest.getInstance("SHA-256");
+            }catch (NoSuchAlgorithmException e){
+                e.printStackTrace();
+            }
             byte[] line;
+            int count = 0;
             while ((line = reader.readLine()) != null){
+                count++;
                 UtilBytes uLine = new UtilBytes(line);
 
                 ArrayList<Integer> indexes = uLine.getAllIndexes(delimiter);
 
                 for (int i=0; i< indexes.size(); i++){
-                    if (i==0){
-                        byte[] email = uLine.getBytesOfRangeLower(0, indexes.get(i));
+                    // Email & password
+                    if (i==0 || i==1){
+                        if (i ==0){
+                            UtilBytes uEmail = new UtilBytes(uLine.getBytesOfRangeLower(0, indexes.get(i)));
 
-                        String sEmail = new String(email);
+                            int atIndex = uEmail.findNextIndex(0, "@".getBytes());
 
-                        int atIndex = sEmail.indexOf('@');
+                            fos.write(new String(uEmail.getBytesOfRange(0, atIndex)).getBytes());                      // local part
+                            fos.write("\t".getBytes());
 
-                        fos.write(sEmail.substring(0, atIndex).getBytes());
-                        fos.write("\t".getBytes());
-                        fos.write(sEmail.substring(atIndex+1, sEmail.length()).getBytes());
-                        fos.write("\t".getBytes());
+                            fos.write(new String(uEmail.getBytesOfRange(atIndex+1, uEmail.size())).getBytes());        // domain part
+                            fos.write("\t".getBytes());
 
-                        fos.write(encoder.encode(email));
+                            fos.write(encoder.encode(uEmail.getData()));                        // B64 email
+                            fos.write("\t".getBytes());
+
+                            if (sha256 != null){
+                                fos.write(bytesToHex(sha256.digest(uEmail.getData())).getBytes());  // sha256
+                            }
+                        }
+
+                        if (i==1){
+                            UtilBytes uPassword = new UtilBytes(uLine.getBytesOfRange(indexes.get(i-1)+delimiter.length, indexes.get(i)));
+
+                            fos.write(encoder.encode(uPassword.getData()));
+                            fos.write("\t".getBytes());
+
+                            if(sha256 != null){
+                                fos.write(bytesToHex(sha256.digest(uPassword.getData())).getBytes());
+                            }
+                        }
                     }else {
-
+                        //fos.write(uLine.getBytesOfRange(indexes.get(i-1)+delimiter.length, indexes.get(i)));
                         fos.write(encoder.encode(uLine.getBytesOfRange(indexes.get(i-1)+delimiter.length, indexes.get(i))));
-
-
                     }
 
                     fos.write(delimiter);
                 }
                 //fos.write(new String(uLine.getBytesOfRange(indexes.get(indexes.size()-1)+delimiter.length, uLine.size()-1)).getBytes());
 
-                fos.write(encoder.encode(uLine.getBytesOfRange(indexes.get(indexes.size()-1)+delimiter.length, uLine.size()-1)));
+                fos.write(uLine.getBytesOfRange(indexes.get(indexes.size()-1)+delimiter.length, uLine.size()-1));   // only hash
                 fos.write("\n".getBytes());
                 fos.flush();
+
+                if (count % 100000 == 0){
+                    System.out.println(count);
+                }
             }
         } catch (IOException e) {
             System.err.println("Output file can't be written to, exiting...");
         }
+
+        System.out.println(System.nanoTime() - start);
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder();
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
     }
 }
